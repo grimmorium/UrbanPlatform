8220fcd4031b5cad5fb3144ca4be56f9245b5ce7
Martin Pawłowski <marcin.pawlowski@aol.pl>
2024-05-27 18:20:07 +0200

    sending full message to Arduino

--- PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
+++ PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
@@ -5,12 +5,14 @@ import time
 import sys
 
 bus = smbus.SMBus(1)
-arduAddress = 0x04              # Arduino I2C Address
+arduAddress1 = 0x04              # Arduino I2C Addresses
+arduAddress2 = 0x05
+arduAddress3 = 0x06
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', 12000))
 
-i2cData = False
+i2cData = 25
 
 
 while True:
@@ -19,14 +21,14 @@ while True:
     
     print("from GUI:" + str(message))
     
-    # send data
-    i2cData = not i2cData
-    bus.write_byte(arduAddress,i2cData)
-    # request data
-    print ("Arduino answer to RPi:", bus.read_byte(arduAddress))
-        
+    for c in list(message):
+        # send data
+        #i2cData = not i2cData
+        bus.write_byte(arduAddress1,c)
+        print ("Arduino answer to RPi:", bus.read_byte(arduAddress1))
+        # request data
+    #print ("Arduino answer to RPi:", bus.read_byte(arduAddress1))
     #time.sleep(1)
-    
     print(message)
 
     server_socket.sendto(message, address)
