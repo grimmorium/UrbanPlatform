0e5296522262b2353007d1f2f64ff4f6f11391f2
Martin Pawłowski <marcin.pawlowski@aol.pl>
2025-03-01 20:18:27 +0100

    Update UrbanPlatformPlatformControler.py

--- PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
+++ PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
@@ -8,7 +8,7 @@ def SendToArd(message, arduAddress):
         # send data
         bus.write_byte(arduAddress,c)
         cr = bus.read_byte(arduAddress)
-        print(str(cr) + " - " + str(cr))
+        print(str(cr) + " - " + chr(cr))
 i=0
 while True:
     i=i+1
