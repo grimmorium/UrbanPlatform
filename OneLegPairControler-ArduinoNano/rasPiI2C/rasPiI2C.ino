6c3872f04a184fd2d27d9279ee3db1c44265aab1
Martin Pawłowski <marcin.pawlowski@aol.pl>
2025-03-02 15:03:23 +0100

    Update rasPiI2C.ino

--- OneLegPairControler-ArduinoNano/rasPiI2C/rasPiI2C.ino
+++ OneLegPairControler-ArduinoNano/rasPiI2C/rasPiI2C.ino
@@ -1,53 +1,89 @@
-/* 
- * Arduino code to send and receive I2C data
- * Tested on Adafruit Feather M0+ Express and Raspberry Pi Model 4
- * 
- * SDA <--> SDA
- * SCL <--> SCL
- * GND <--> GND
- * 
- * Sets built-in LED (1 = on, 0 = off) on Feather when requested 
- * and responds with data received
- */
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x04       // I2C address for Arduino
-#define LED 13                   // Built-in LED
 int i2cData = 0;                 // the I2C data received
+
+char charAr[11];
+
+byte charCnt = -1;
+
+char msgType = ' ';
+
+short AdjS1=0;
+short AdjS2=0;
+short AdjS3=0;
+short AdjS4=0;
+short AdjS5=0;
+short AdjS6=0;
+short AdjDC=0;
+
+byte S1=180;
+byte S2=180;
+byte S3=180;
+byte S4=180;
+byte S5=180;
+byte S6=180;
+byte DC=180;
+
 void setup(){
   Serial.begin(115200);
 
   Wire.begin(SLAVE_ADDRESS);
+  Wire.setWireTimeout(200,false);
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, HIGH);
 }
 void loop() {
   // Everything happens in the interrupts
 }
 // Handle reception of incoming I2C data
 void receiveData(int byteCount) {
+
+  
   while (Wire.available()) {
     i2cData = Wire.read();
+    if(i2cData == '<'){charCnt = 1;}
+    if(charCnt = 2){msgType = i2cData;}
 
+    if(charCnt = 3){S1 = i2cData;}
+    if(charCnt = 4){S2 = i2cData;}
+    if(charCnt = 5){S3 = i2cData;}
+    if(charCnt = 6){S4 = i2cData;}
+    if(charCnt = 7){S5 = i2cData;}
+    if(charCnt = 8){S6 = i2cData;}
+    if(charCnt = 9){DC = i2cData;}
+
+    if(i2cData == '>'){charCnt = -1; setMotoServoStates();}
+    if(charCnt > 1){charCnt++;}
+    
   }
+  
 }
+
+void setMotoServoStates(){
+  Serial.print(msgType);
+  Serial.print(" - ");
+  Serial.println("setMotoServoStates IN");
+
+  msgType = ' ';
+  AdjS1=0;
+  AdjS2=0;
+  AdjS3=0;
+  AdjS4=0;
+  AdjS5=0;
+  AdjS6=0;
+  AdjDC=0;
+  S1=180;
+  S2=180;
+  S3=180;
+  S4=180;
+  S5=180;
+  S6=180;
+  DC=0;
+}
+
 // Handle request to send I2C data
 void sendData() { 
-  Wire.write(1);
-  Serial.write('1');
-  Wire.write(2);
-  Serial.write('2');
-  Wire.write(3);
-  Serial.write('3');
-  Wire.write(i2cData);
-  Serial.write(int(i2cData));
-  Wire.write(3);
-  Serial.write('3');
-  Wire.write(2);
-  Serial.write('2');
-  Wire.write(1);
-  Serial.write('1');
-  Serial.write(char(10));
-  Serial.write(char(13));
+
+  //Serial.write(char(10));
+  //Serial.write(char(13));
 }
