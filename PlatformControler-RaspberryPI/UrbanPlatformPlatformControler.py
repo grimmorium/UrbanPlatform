a48e5efd5e0c20e4d9e9dea466830b875389f321
Martin Pawłowski <marcin.pawlowski@aol.pl>
2025-03-04 17:07:06 +0100

    Update UrbanPlatformPlatformControler.py

--- PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
+++ PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
@@ -14,19 +14,19 @@ def SendToArd_block(message, arduAddress):
     # send data
     bus.write_i2c_block_data(arduAddress,0,list(message))
     cr = bus.read_i2c_block_data(arduAddress,0,11)
-    print(cr)
+    #print(cr)
         
 i=0
 #msg="just a test"
 msg = [ord('<'),ord('S'),1,2,3,4,5,6,7,ord('>')];
 
 while True:
-    i=i+1
-    if i==256:
+    i=i+5
+    if i>255:
         i=0
         
     msg = [ord('<'),ord('S'),i,i,i,i,i,i,i,ord('>')]
     
     SendToArd_block(msg, 4)
     print("sent")
-    time.sleep(0.1)
+    time.sleep(0.05)
