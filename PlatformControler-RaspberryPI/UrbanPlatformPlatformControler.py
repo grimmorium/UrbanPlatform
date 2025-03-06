34a62de930b05821b9d1e910474a808db91fb9d6
Martin Pawłowski <marcin.pawlowski@aol.pl>
2025-03-06 18:25:54 +0100

    Update UrbanPlatformPlatformControler.py

--- PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
+++ PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
@@ -29,21 +29,52 @@ while True:
     print("from GUI:" + message_str)
     server_socket.sendto(message, address)
     
-    #print(message_str[0:1])
-    #print(message_str[1:2])
+    rideWalk = message_str[2:3] #ride / walk
+    lowMidleHigh = message_str[3:4] #low / midle / high
+    cross = message_str[4:5] #cross
+    F1 = message_str[5:6] #F1
+    F2 = message_str[6:7] #F2
+    F3 = message_str[7:8] #F3
     
-    print(message_str[2:3]) #ride / walk
-    print(message_str[3:4]) #low / midle / high
-    print(message_str[4:5]) #cross
-    print(message_str[5:6]) #F1
-    print(message_str[6:7]) #F2
-    print(message_str[7:8]) #F3
-    #print(message_str[8:9])
-    #print(message_str[9:10])
+    print(rideWalk) #ride / walk
+    print(lowMidleHigh) #low / midle / high
+    print(cross) #cross
+    print(F1) #F1
+    print(F2) #F2
+    print(F3) #F3
+
+    rideWalkM=lowMidleHighM=1
+    S1=S2=S3=S4=S5=S6=DC1=DC2=0
     
+    if F3=="1":
+        S1=5
+    if F3=="2":
+        S2=5
+    if F3=="3":
+        S3=5
+    if F3=="4":
+        S4=5
+    if F3=="5":
+        S5=5
+    if F3=="6":
+        S6=5
+    
+    if rideWalk=="W":
+        rideWalkM = 2
+    if lowMidleHigh=="M":
+        lowMidleHighM = 2
+    if lowMidleHigh=="H":
+        lowMidleHighM = 5
+       
+    S1 = S1 * rideWalkM * lowMidleHighM
+    S2 = S2 * rideWalkM * lowMidleHighM
+    S3 = S3 * rideWalkM * lowMidleHighM
+    S4 = S4 * rideWalkM * lowMidleHighM
+    S5 = S5 * rideWalkM * lowMidleHighM
+    S6 = S6 * rideWalkM * lowMidleHighM
     
     i=5
-    msg = [ord('<'),ord('S'),i,i+1,i+2,i+3,i+4,i+5,i+6,i+7,ord('>')]
+    msg = [ord('<'),ord('S'),S1,S2,S3,S4,S5,S6,DC1,DC2,ord('>')]
     
     SendToArd_block(msg, 4)
     print("sent")
