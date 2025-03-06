291e5573ee4834cbec6a92553b7415ebb739e2c6
Martin Pawłowski <marcin.pawlowski@aol.pl>
2025-03-06 17:06:00 +0100

    TCP communication RPi<<>>PC App part

---
+++ PlatformControler-RaspberryPI/UrbanPlatformPlatformControler.py
@@ -0,0 +1,12 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('', 12000))
+
+
+while True:
+    message, address = server_socket.recvfrom(1024)
+    message = message.upper()
+    
+    print("from GUI:" + str(message))
+    server_socket.sendto(message, address)
